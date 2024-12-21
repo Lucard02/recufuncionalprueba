00952db649ffb63f72e9000f9ec0c6d3308b4761
IgnacioTVila <ivila@frba.utn.edu.ar>
2024-12-21 11:04:33 -0300

    commiteo 2

--- src/Library.hs
+++ src/Library.hs
@@ -9,48 +9,57 @@ data Aventurero = UnAventurero{
     carga :: Number,
     salud :: Number,
     coraje :: Bool,
-    criterioDeSeleccionDeEncuentros :: Encuentro 
+    criterio :: Criterio 
 } Deriving (Show,Eq)
 
 av1 :: Aventrurero
-av1 = UnAventurero "Lucas" 10 100 True ...
+av1 = UnAventurero "Lucas" 10 100 True conformista
 
 av2 :: Aventrurero
-av2 = UnAventurero "Matias" 20 50 False ...
+av2 = UnAventurero "Matias" 20 50 False valiente
 
 av3 :: Aventrurero
-av3 = UnAventurero "Pepe" 30 70 True ...
+av3 = UnAventurero "Pepe" 30 70 True lightPacker
 
-type Criterio = Aventurero -> Bool
+--type Criterio = Aventurero -> Encuentro -> Bool
+type Criterio = Aventurero -> Encuentro -> Bool
 
 conformista :: Criterio
-conformista 
+conformista _ _ = True
 
-valiente :: 
+valiente :: Criterio
+valiente aventurero encuentro
+    | aventurero = 
+
+--estaConforme :: Aventrurero -> Encuentro -> Bool
+--estaConforme aventurero encuentro = 
 
 --2a
 
-type Aventureros = [Aventrurero]
+type Aventureros = [Aventurero]
 
-aventrureros :: aventrureros
-aventrureros = [av1,av2]
+aventureros :: Aventureros
+aventureros = [av1,av2,av3]
 
-nombreMasDe5 :: Aventrureros -> Bool
-nombreMasDe5 aventrureros = any masDe5 aventrureros
+nombreMasDe5 :: Aventureros -> Bool
+nombreMasDe5 aventureros = any masDe5 aventureros
 
-masDe5 :: Aventrurero -> Bool
-masDe5 aventrurero = ( length nombre aventrurero) > 5
+masDe5 :: Aventurero -> Bool
+masDe5 aventrurero = ( length nombre aventurero) > 5
 --2b
 
-cargaTotal :: Aventrureros -> Number
-cargaTotal aventrureros = sum (filter cargaPar) aventrureros
+cargaTotal :: Aventureros -> Number
+cargaTotal aventureros = sum (filter cargaPar) aventureros
 
-cargaPar :: Aventrurero -> Bool
-cargaPar aventurero = even (carga aventrurero)
+cargaPar :: Aventurero -> Bool
+cargaPar aventurero = even (carga aventurero)
 
 --3
 
-type Encuentro = Aventrurero -> Aventrurero 
+type Encuentro = Aventurero -> Aventurero 
+
+encuentroConPersonaje :: Encuentro -> Aventurero -> Aventurero
+encuentroConPersonaje personaje aventrurero = reducirCarga 1 . personaje aventurero 
 
 curandero :: Encuentro
 curandero  aventrurero = aumentarSalud 20 . reducirCarga (carga aventrurero / 2)
@@ -61,14 +70,23 @@ inspirador = aumentarSalud 10 . otorgarCoraje
 embaucador :: Encuentro
 embaucador = cambiarCriterio (lightPacker) . aumentarSaludPorcentual (-50). reducirCarga (-10) . (not otorgarCoraje)
 
-otorgarCoraje :: Aventrurero -> Aventrurero
+otorgarCoraje :: Aventrurero -> Aventurero
 otorgarCoraje aventurero = aventrurero{coraje = True}
 
-reducirCarga :: Number -> Aventrurero -> Aventrurero
+reducirCarga :: Number -> Aventrurero -> Aventurero
 reducirCarga num aventurero = aventrurero{carga = carga aventrurero - num}
 
-aumentarSaludPorcentual :: Number -> Aventrurero -> Aventrurero
+aumentarSaludPorcentual :: Number -> Aventurero -> Aventurero
 aumentarSaludPorcentual num aventrurero = aventrurero{salud = salud aventrurero + (salud aventrurero) * (num/100)}
 
-cambiarCriterio :: Criterio -> Aventrurero -> Aventrurero
+cambiarCriterio :: Criterio -> Aventurero -> Aventurero
 cambiarCriterio criterioNuevo aventrurero = aventrurero{criterio = criterioNuevo }
+
+--4
+
+type Encuentros = [Encuentro]
+
+encuentros :: Encuentros
+encuentros = [curandero,inspirador,embaucador]
+
+
